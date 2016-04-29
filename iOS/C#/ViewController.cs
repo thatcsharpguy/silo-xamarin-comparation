9c5dae3ad5ab795e5fa6068857664a16986b028d
Antonio Feregrino <antonio.feregrino@gmail.com>
2016-04-29 16:03:56 -0500

    C# stuff.

---
+++ iOS/C#/ViewController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+using UIKit;
+
+namespace Xevensthein
+{
+    public partial class ViewController : UIViewController, IUITextFieldDelegate
+    {
+        public ViewController(IntPtr handle)
+            : base(handle)
+        {
+           
+        }
+
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+
+            FirstWordTextField.Delegate = this; 
+            SecondWordTextField.Delegate = this;
+
+            CompareButton.TouchUpInside += CompareButtonClicked;
+        }
+
+
+        [Foundation.Export("textFieldShouldReturn:")]
+        public bool ShouldReturn(UIKit.UITextField textField)
+        {
+            if (textField == FirstWordTextField)
+            {
+                FirstWordTextField.ResignFirstResponder();
+                SecondWordTextField.BecomeFirstResponder();
+            }
+            else
+            {
+                SecondWordTextField.ResignFirstResponder();
+            }
+            return true;
+        }
+
+        void CompareButtonClicked (object sender, EventArgs e)
+        {
+            ResultLabel.Text = "Hola";
+        }
+
+        public override void DidReceiveMemoryWarning()
+        {
+            base.DidReceiveMemoryWarning();
+            // Release any cached data, images, etc that aren't in use.
+        }
+    }
+}
+
---
+++ iOS/C#/ViewController.designer.cs
@@ -0,0 +1,54 @@
+// WARNING
+//
+// This file has been generated automatically by Xamarin Studio from the outlets and
+// actions declared in your storyboard file.
+// Manual changes to this file will not be maintained.
+//
+using Foundation;
+using System;
+using System.CodeDom.Compiler;
+using UIKit;
+
+namespace Xevensthein
+{
+	[Register ("ViewController")]
+	partial class ViewController
+	{
+		[Outlet]
+		[GeneratedCode ("iOS Designer", "1.0")]
+		UIButton CompareButton { get; set; }
+
+		[Outlet]
+		[GeneratedCode ("iOS Designer", "1.0")]
+		UITextField FirstWordTextField { get; set; }
+
+		[Outlet]
+		[GeneratedCode ("iOS Designer", "1.0")]
+		UILabel ResultLabel { get; set; }
+
+		[Outlet]
+		[GeneratedCode ("iOS Designer", "1.0")]
+		UITextField SecondWordTextField { get; set; }
+
+
+		void ReleaseDesignerOutlets ()
+		{
+			if (CompareButton != null) {
+				CompareButton.Dispose ();
+				CompareButton = null;
+			}
+			if (FirstWordTextField != null) {
+				FirstWordTextField.Dispose ();
+				FirstWordTextField = null;
+			}
+			if (ResultLabel != null) {
+				ResultLabel.Dispose ();
+				ResultLabel = null;
+			}
+			if (SecondWordTextField != null) {
+				SecondWordTextField.Dispose ();
+				SecondWordTextField = null;
+			}
+		}
+	}
+}
