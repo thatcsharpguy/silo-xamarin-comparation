edc8ba22d072f23c23fa85e47d44a0ad1baa9a71
Antonio Feregrino <antonio.feregrino@gmail.com>
2016-05-02 22:53:46 -0500

    C#!

---
+++ Android/C#/Droid/MainActivity.cs
@@ -0,0 +1,23 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+
+namespace Xevensthein
+{
+    [Activity(Label = "Xev C#", MainLauncher = true, Icon = "@mipmap/icon")]
+    public class MainActivity : Activity
+    {
+        int count = 1;
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+
+        }
+    }
+}
+
+
