70571bea8e14e2d33196a7511c17b08ecf77a577
Antonio Feregrino <antonio.feregrino@gmail.com>
2016-05-03 21:29:17 -0500

    C# Android App

--- Android/C#/Droid/MainActivity.cs
+++ Android/C#/Droid/MainActivity.cs
@@ -3,6 +3,8 @@ using Android.Widget;
 using Android.OS;
 using Android.Support.V7.App;
 
+using Xevensthein.Core;
+
 namespace Xevensthein
 {
     [Activity(Label = "Xev C#", 
@@ -11,15 +13,25 @@ namespace Xevensthein
         Icon = "@mipmap/icon")]
     public class MainActivity : AppCompatActivity
     {
-        int count = 1;
+        Button _compareButton;
+        TextView _resultTextView;
+        EditText _firstWordEditText;
+        EditText _secondWordEditText;
 
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
-
-            // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.Main);
 
+            _compareButton = FindViewById<Button>(Resource.Id.compareButton);
+            _firstWordEditText = FindViewById<EditText>(Resource.Id.firstWordEditText);
+            _secondWordEditText = FindViewById<EditText>(Resource.Id.secondWordEditText);
+            _resultTextView = FindViewById<TextView>(Resource.Id.resultTextView);
+
+            _compareButton.Click += (sender, e) => {
+                var res = LevenshteinDistance.Compute(_firstWordEditText.Text,_secondWordEditText.Text);
+                _resultTextView.Text  = res.ToString();
+            };
         }
     }
 }
---
+++ Android/C#/Wear/MainActivity.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Support.V4.App;
+using Android.Support.Wearable.Views;
+using Android.Views;
+using Android.Widget;
+using Xevensthein.Wear.Resources;
+
+namespace Xevensthein.Wear
+{
+    [Activity(Label = "Xevensthein.Wear", MainLauncher = true, Icon = "@mipmap/icon")]
+    public class MainActivity : Activity
+    {
+
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+        }
+    }
+}
+
+
+
